9524386182d886f43b8e8c7288e1d3c823f1aec1
JuniorCarandina <carandinajunior@gmail.com>
2026-03-17 11:03:16 -0300

    Create teste.js

---
+++ pages/teste.js
@@ -0,0 +1,15 @@
+export default function Teste() {
+  return (
+    <div style={{ 
+      background: '#0a1423', 
+      minHeight: '100vh', 
+      display: 'flex', 
+      alignItems: 'center', 
+      justifyContent: 'center',
+      color: '#f59e0b',
+      fontSize: '24px'
+    }}>
+      ✅ FUNCIONOU!
+    </div>
+  )
+}
